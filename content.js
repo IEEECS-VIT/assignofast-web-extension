4ec97d36ce0353c30516b15ee5f70fd9a24c100e
Akshit <akshit10.anand2004@gmail.com>
2024-09-03 22:43:26 +0530

    function is now async

--- content.js
+++ content.js
@@ -27,55 +27,62 @@ let da_page = async () => {
         let csrf;
         try { csrf = document.getElementsByName("_csrf")[3].defaultValue; } catch { }
 
+        // Create an array to hold all fetch promises
+        let fetchPromises = [];
+
         for (let i = 1; i < table.children.length; i++) {
             let classid = table.children[i].children[1].innerHTML;
 
             if (table.children[i].children[3].children.length != 1) {
-                let response = await fetch(
-                    `${window.location.origin}/vtop/examinations/processDigitalAssignment`,
-                    {
+                // Create a fetch promise and push it to the array
+                fetchPromises.push(
+                    fetch(`${window.location.origin}/vtop/examinations/processDigitalAssignment`, {
                         method: "POST",
                         headers: {
                             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
                             "X-Requested-With": "XMLHttpRequest",
                         },
                         body: `authorizedID=${scrapedData.reg_no}&x=${new Date().toGMTString()}&classId=${classid}&_csrf=${csrf}`,
-                    }
-                );
+                    }).then(response => response.text())
+                        .then(data => {
+                            let parser = new DOMParser();
+                            let doc = parser.parseFromString(data, "text/html");
+                            let table_inner = doc.getElementsByClassName("fixedContent tableContent");
+                            let course_code = doc.getElementsByClassName("fixedContent tableContent")[0].children[1].innerText;
+                            let course_title = doc.getElementsByClassName("fixedContent tableContent")[0].children[2].innerText;
 
-                let data = await response.text();
-                let parser = new DOMParser();
-                let doc = parser.parseFromString(data, "text/html");
-                let table_inner = doc.getElementsByClassName("fixedContent tableContent");
-                let course_code = doc.getElementsByClassName("fixedContent tableContent")[0].children[1].innerText;
-                let course_title = doc.getElementsByClassName("fixedContent tableContent")[0].children[2].innerText;
-
-                let due_dates = [];
-                Array.from(table_inner).forEach((row) => {
-                    let date = row.childNodes[9].childNodes[1];
-                    let is_uploaded = true;
-                    let assessment_title = row.children[1].innerText; // Scraping assessment title
-                    try {
-                        is_uploaded = row.children[6].children[0].innerHTML === "";
-                    } catch { }
-                    try {
-                        if (date.style.color == "green" && is_uploaded) {
-                            due_dates.push({
-                                assessment_title: assessment_title, // Adding assessment title
-                                date_due: date.innerHTML
+                            let due_dates = [];
+                            Array.from(table_inner).forEach((row) => {
+                                let date = row.childNodes[9].childNodes[1];
+                                let is_uploaded = true;
+                                let assessment_title = row.children[1].innerText; // Scraping assessment title
+                                try {
+                                    is_uploaded = row.children[6].children[0].innerHTML === "";
+                                } catch { }
+                                try {
+                                    if (date.style.color == "green" && is_uploaded) {
+                                        due_dates.push({
+                                            assessment_title: assessment_title, // Adding assessment title
+                                            date_due: date.innerHTML
+                                        });
+                                    }
+                                } catch { }
                             });
-                        }
-                    } catch { }
-                });
 
-                scrapedData.courses.push({
-                    class_id: classid,
-                    course_code: course_code,
-                    course_title: course_title,
-                    due_dates: due_dates
-                });
+                            return {
+                                class_id: classid,
+                                course_code: course_code,
+                                course_title: course_title,
+                                due_dates: due_dates
+                            };
+                        })
+                );
             }
         }
+
+        // Wait for all fetches to complete
+        let courses = await Promise.all(fetchPromises);
+        scrapedData.courses = courses;
 
         console.log(scrapedData);
 
