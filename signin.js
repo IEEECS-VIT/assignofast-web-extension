5943b0cfe1d2800f900488ff26156cc7ba931670
Amrit Sundarka <amritsundarka2004@gmail.com>
2024-09-20 10:23:03 +0530

    Working on Login

--- signin.js
+++ signin.js
@@ -16,12 +16,12 @@ document.addEventListener('DOMContentLoaded', function () {
                     const user = result.user;
                     const uid = user.uid;
 
-                    // Store the UID in Chrome storage
-                    chrome.storage.local.set({ uid: uid }, function () {
-                        console.log('UID saved:', uid);
+                    // Store the UID and set a flag in Chrome storage
+                    chrome.storage.local.set({ uid: uid, justSignedIn: true }, function () {
+                        console.log('UID saved and justSignedIn flag set:', uid);
 
                         // Close the sign-in window
-                        // window.close();
+                        window.close();
                     });
                 })
                 .catch((error) => {
